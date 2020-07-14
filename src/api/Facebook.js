bb9de897cfa5d8db9dad4b0f721a418b66cf33db
Okekeprince1 <okeke.p.ebube@gmail.com>
2020-07-14 09:25:04 +0100

    Used Facebook service to generate token

---
+++ src/api/Facebook.js
@@ -0,0 +1,29 @@
+import  Constants  from 'expo-constants';
+import * as Facebook from 'expo-facebook';
+
+const permissions = ['public_profile', 'email'];
+
+const appName = "InStore";
+const appId = "284670935860439";
+
+const loginAsync = async () => {
+    try {
+        await Facebook.initializeAsync(appId, appName);
+        const { type, token } = await Facebook.logInWithReadPermissionsAsync(Constants.manifest.facebookAppId, { permissions });
+
+        if (type === 'success') {
+            return Promise.resolve(token);
+        }
+
+        if (type === 'error') {
+            return Promise.reject('No success');
+        }
+
+    } catch (error) {
+        return Promise.reject(error);
+    }
+}
+
+export const FacebookApi = {
+    loginAsync
+}
